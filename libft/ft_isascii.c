86847b27ba1c3ce8fb839df22dea94c40f7ba61c
katataku <katataku.dev@gmail.com>
2021-10-07 18:52:49 +0900

    wip

---
+++ libft/ft_isascii.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+int	ft_isascii(int c)
+{
+	int	ans;
+
+	ans = FALSE;
+	if (0 <= c && c <= 127 )
+	{
+		ans = TRUE;
+	}
+	return (ans);
+}
---
+++ libft/libft.h
@@ -0,0 +1,21 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include "stdlib.h"
+void	*ft_memset(void *buf, int ch, size_t n);
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+
+# define TRUE 1
+# define FALSE 0
+#endif
