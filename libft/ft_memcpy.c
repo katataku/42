7c1984be43d2525f3a0abcd9398751757f99e6f7
katataku <katataku.dev@gmail.com>
2021-10-07 20:26:06 +0900

    wip

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,28 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 18:45:53 by takkatao          #+#    #+#             */
-/*   Updated: 2021/10/07 19:59:47 by takkatao         ###   ########.fr       */
+/*   Updated: 2021/10/07 20:24:41 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *buf, int ch, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	unsigned char	*ptr;
+	char	*ptr_dst;
+	char	*ptr_src;
 
-	ptr = (unsigned char *) buf;
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	ptr_dst = (char *)dst;
+	ptr_src = (char *)src;
 	i = 0;
 	while (i < n)
 	{
-		ptr[i] = ch;
+		ptr_dst[i] = ptr_src[i];
 		i++;
 	}
-	return (buf);
+	return (dst);
 }
