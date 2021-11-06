7e8db44a594bba78f704e21d231bdc110b5828f8
katataku <katataku.dev@gmail.com>
2021-11-06 19:34:54 +0900

    fix strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 10:47:31 by takkatao          #+#    #+#             */
-/*   Updated: 2021/11/06 09:48:59 by takkatao         ###   ########.fr       */
+/*   Updated: 2021/11/06 19:33:50 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strrchr(const char *s, int c)
 {
 	char	*ans_ptr;
 
-	ans_ptr = (char *)(s + ft_strlen(s) + 1);
+	ans_ptr = (char *)(s + ft_strlen(s));
 	while (true)
 	{
 		if (*ans_ptr == (char)c)
