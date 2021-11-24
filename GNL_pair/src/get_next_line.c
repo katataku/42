f10d24e052fc717357dbdcf50b51e2bd588b3d85
katataku <katataku.dev@gmail.com>
2021-11-24 15:42:51 +0900

    wip main

--- GNL_pair/src/get_next_line.c
+++ GNL_pair/src/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 10:08:11 by takkatao          #+#    #+#             */
-/*   Updated: 2021/11/22 15:47:22 by takkatao         ###   ########.fr       */
+/*   Updated: 2021/11/23 09:58:46 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,29 +30,39 @@ char	*get_next_line_core(int fd, size_t buffer_size)
 	static char *ans;
 	ssize_t	rc;
 	char	*next_n_ptr;
+	char	*tmp;
+	size_t	next_n_index;
 
 	rc = 1;
-	ans = "";
 	while (true)
 	{
 		if (buffer == NULL)
 		{
 			buffer = (char *)malloc(buffer_size + 1);
+			if (buffer == NULL)
+				return (NULL);
 			rc = read(fd, buffer, buffer_size);
 			if (rc < 0)
 				return (NULL);
 			if (rc == 0)
 				break ;
-			buffer[buffer_size] = '\0';//a0
+			buffer[buffer_size] = '\0';
+			next_n_index = 0;
 		}
-		ans = ft_strjoin(ans, buffer);
-		if (strchr(buffer, '\n'))//次のためにbufferを整理する
+		if (ans == NULL)
+			ans = "";
+		tmp = ft_strjoin(ans, &buffer[next_n_index]);
+		free(ans);
+		ans = tmp;
+		if (strchr(buffer, '\n'))
 		{
-			buffer = strchr(buffer, '\n') + 1;
-			next_n_ptr = strchr(ans, '\n') + 1;
-			*next_n_ptr = '\0';
+			next_n_ptr = strchr(buffer, '\n') + 1;//次のためにbufferを整理
+			next_n_index = next_n_ptr - buffer;
+			next_n_ptr = strchr(ans, '\n') + 1;//今回のためにansを整理
+			*next_n_ptr = '\0';//今回のためにansを整理
 			break ;
 		}
+		free (buffer);
 		buffer = NULL;
 	}
 	return (ans);
@@ -66,7 +76,9 @@ char	*get_next_line(int fd)
 int	main(void)
 {
 	int	fd;
-	
+
+
+
 	write(1,"1",1);
 	fd = open("../test/inputs/test_1_a", O_RDONLY);
 	assert(strcmp(get_next_line(fd), "a") == 0);
@@ -95,11 +107,11 @@ int	main(void)
 	assert(strcmp(get_next_line_core(fd, 5), "b\n") == 0);
 	assert(strcmp(get_next_line_core(fd, 5), "c\n") == 0);
 
-	write(1,"7",1);
-	fd = open("../test/inputs/test_empty", O_RDONLY);
-	assert(get_next_line_core(fd, 5) == NULL);
+	// write(1,"7",1);
+	// fd = open("../test/inputs/test_empty", O_RDONLY);
+//	assert(get_next_line_core(fd, 5) == NULL);
 
-	printf("All Green\n");
+	printf("\nAll Green\n");
 
 /*
 	todo:
