b1c970ee9bb221510fa5c420332a41c48a76b7fc
katataku <katataku.dev@gmail.com>
2021-12-04 19:15:54 +0900

    wip b2b

--- born2beroot/ansible/files/monitor.sh
+++ born2beroot/ansible/files/monitor.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 arc=$(uname -a)
-pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+pcpu=$(grep "physical id" /proc/cpuinfo | wc -l)
 vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
-fram=$(free -m | awk '$1 == "Mem:" {print $2}')
-uram=$(free -m | awk '$1 == "Mem:" {print $3}')
-pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
-fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
-udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
-pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
+total_ram=$(free -m | awk '$1 == "Mem:" {print $2}')
+used_ram=$(free -m | awk '$1 == "Mem:" {print $3}')
+percent_ram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot' | awk '{ft += $2} END {print ft}')
+udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot' | awk '{ut += $3} END {print ut}')
+pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
 cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+lb=$(who -b | awk '{print $2 " " $3}')
 lvmt=$(lsblk | grep "lvm" | wc -l)
 lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
 #You need to install net tools for the next step [$ sudo apt install net-tools]
-ctcp=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
+ctcp=$(cat /proc/net/sockstat | awk '$1 == "TCP:" {print $3}')
 ulog=$(users | wc -w)
 ip=$(hostname -I | awk '$1')
 mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
@@ -22,7 +22,7 @@ cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l) # journalctl should be runn
 wall "	#Architecture: $arc
 	#CPU physical: $pcpu
 	#vCPU: $vcpu
-	#Memory Usage: $uram/${fram}MB ($pram%)
+	#Memory Usage: $used_ram/${total_ram}MB ($percent_ram%)
 	#Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
 	#CPU load: $cpul
 	#Last boot: $lb
