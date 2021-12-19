a85d1fc677238a6f124de4afc978e942bf3e9d16
katataku <katataku.dev@gmail.com>
2021-12-19 11:13:21 +0900

    wip ex02

---
+++ CPP-Module-02/ex02/includes/Fixed.hpp
@@ -0,0 +1,39 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <cmath>
+#include "escape_sequence.hpp"
+#include <iostream>
+#include <string>
+#define EIGHT_BIT 256
+class Fixed
+{
+  private:
+	int				 value;
+	static const int numFactorialBit = 8;
+
+  public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &);
+	Fixed(const int val);
+	Fixed(const float val);
+
+	Fixed &operator=(const Fixed &);
+	bool operator<(const Fixed &);
+	bool operator>(const Fixed &);
+	bool operator<=(const Fixed &);
+	bool operator>=(const Fixed &);
+	bool operator==(const Fixed &);
+	bool operator!=(const Fixed &);
+	
+
+	int	 getRawBits(void) const;
+	void setRawBits(int const raw);
+
+	float toFloat(void) const;
+	int	  toInt(void) const;
+
+};
+
+std::ostream& operator<<(std::ostream& , const Fixed& );
+#endif
