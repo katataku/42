a5016bd5aa91bd9bb11de7da8b9731223d28723b
katataku <katataku.dev@gmail.com>
2021-12-19 10:46:03 +0900

    finish ex01

---
+++ CPP-Module-02/ex01/src/Fixed.cpp
@@ -0,0 +1,58 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &f)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->setRawBits(f.getRawBits());
+}
+
+Fixed::Fixed(const int val)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->setRawBits(val * EIGHT_BIT);
+}
+Fixed::Fixed(const float val)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->setRawBits(roundf(val * EIGHT_BIT));
+}
+
+Fixed &Fixed::operator=(Fixed const &f)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->setRawBits(f.getRawBits());
+	return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+	return this->value;
+}
+void Fixed::setRawBits(int const raw)
+{
+	this->value = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return (float) this->getRawBits() / EIGHT_BIT;
+}
+int Fixed::toInt(void) const
+{
+	return this->value / EIGHT_BIT;
+}
+
+std::ostream &operator<<(std::ostream &os, const Fixed &f)
+{
+	os << f.toFloat();
+	return os;
+}
