1f843af75aa7301afb450d0e9c311b2a34142002
katataku <katataku.dev@gmail.com>
2022-06-09 14:16:09 +0900

    Inception wordpress start.shの再インストール防止

--- Inception/srcs/requirements/wordpress/srcs/start.sh
+++ Inception/srcs/requirements/wordpress/srcs/start.sh
@@ -1,25 +1,34 @@
 
-wp core download --allow-root
 
-wp config create \
-	--allow-root \
-	--dbname=${MARIADB_DATABASE} \
-	--dbuser=${MARIADB_USER} \
-	--dbpass=${MARIADB_PASSWORD} \
-	--dbhost=${WORDPRESS_DB_HOST}
-echo "finish config"
+if [ ! -f ./wp-config.php ]; then
+	echo "WordPress is not installed"
+	echo "start download WordPress"
+	wp core download --allow-root
 
-wp core install \
-	--allow-root \
-	--url=localhost \
-	--title=Example \
-	--admin_user=supervisor \
-	--admin_password=strongpassword \
-	--admin_email=info@example.com
+	echo "start create config of WordPress"
+	wp config create \
+		--allow-root \
+		--dbname=${MARIADB_DATABASE} \
+		--dbuser=${MARIADB_USER} \
+		--dbpass=${MARIADB_PASSWORD} \
+		--dbhost=${WORDPRESS_DB_HOST}
 
-chown -R www-data:www-data *
-chmod -R 755 /var/www/*
+	echo "start install WordPress"
+	wp core install \
+		--allow-root \
+		--url=localhost \
+		--title=Example \
+		--admin_user=supervisor \
+		--admin_password=strongpassword \
+		--admin_email=info@example.com
 
-echo "finish setup"
+	chown -R www-data:www-data *
+	chmod -R 755 /var/www/*
 
+	echo "finish install"
+else
+	echo "WordPress is already installed"
+fi
+
+echo "Starting pfp-fpm7.3"
 php-fpm7.3 -F
